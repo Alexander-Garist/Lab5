f80743092a3d11092d354dc2020a7ab2a4c2fbb7
Alexander Garist <garist.alexander@gmail.com>
2020-12-12 20:52:43 +0300

    first commit Lab5

---
+++ src/root/Main.java
@@ -0,0 +1,10 @@
+package root;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(3);
+        frame.setVisible(true);
+    }
+}
